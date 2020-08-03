9e2ce62882cbeaef9c08564825e4508a2406a71a
Sandeep K S <61325558+sandeepks011999@users.noreply.github.com>
2020-08-03 11:35:37 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,40 @@
+makeCacheMatrix <- function(x = matrix()) {
+m<-NULL   
+evn <- environment()  
+y<-NULL 
+
+setmatrix<-function(y){  
+	x<<-y  
+	m<<-NULL 	}
+  
+getmatrix<-function() x  
+setinverse<-function(solve) m<<- solve  
+getinverse<-function() m  
+getenv<- function() environment()
+
+list (setmatrix=setmatrix, getmatrix = getmatrix,   
+setinverse = setinverse,
+getinverse = getinverse,
+getenv = getenv)
+
+}
+
+cacheSolve <- function(xMat= m(), ...) {
+	
+	m <- xMat$getinverse() 
+	if(!is.null(m)){ 
+		if(xMat$setmatrix() == xMat$getmatrix()) { 
+    	message("getting cached data")
+    	matrix<-xMat$get()
+    	m<-solve(matrix, ...)
+    	xMat$setmatrix(m)
+    	return(m) 
+    	} 
+    	y <- xMat$getmatrix() 
+    	xMat$setmatrix(y) 
+    	m <- solve(y, ...) 
+    	xMat$setinverse(m) 
+    	m 
+    	}
+    	
+}
